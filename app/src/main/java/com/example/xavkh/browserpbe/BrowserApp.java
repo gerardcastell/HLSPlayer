bc97d62863066bc7236f220bba0634933200f929
xavitordera <xavkhinson@hotmail.com>
2017-11-22 16:13:06 +0100

    Primer commit

--- app/src/main/java/com/example/xavkh/browserpbe/BrowserApp.java
+++ app/src/main/java/com/example/xavkh/browserpbe/BrowserApp.java
@@ -3,6 +3,7 @@ package com.example.xavkh.browserpbe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class BrowserApp extends AppCompatActivity {
@@ -12,5 +13,10 @@ public class BrowserApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser_app);
         WebView webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl("https://www.google.com");
     }
 }
