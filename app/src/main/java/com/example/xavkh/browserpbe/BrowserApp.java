477cffca394259101a82ec9eacb7faf8ce34f56c
xavitordera <xavkhinson@hotmail.com>
2017-11-29 17:46:28 +0100

    yey

--- app/src/main/java/com/example/xavkh/browserpbe/BrowserApp.java
+++ app/src/main/java/com/example/xavkh/browserpbe/BrowserApp.java
@@ -2,21 +2,46 @@ package com.example.xavkh.browserpbe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.Button;
+
 
 
 public class BrowserApp extends AppCompatActivity {
 
+    EditText url;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser_app);
-        WebView webView = findViewById(R.id.webView);
+        url = findViewById(R.id.edittext);
+        Button mButton = (Button) findViewById(R.id.button);
+        webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
-        webView.loadUrl("192.168.1.100");
+        webView.loadUrl("http://www.google.es/");
+
+        mButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        webView.loadUrl(url.getText().toString());
+                    }
+                }
+        );
+        webView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
     }
 }
