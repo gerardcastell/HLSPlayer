a8b17d793d9e25d40846fab49ee67440e18624e2
Gerard Castell <gerard@Air-de-Gerard.home>
2018-09-16 13:40:02 +0200

    first commit

--- app/src/main/java/com/example/xavkh/browserpbe/Reproductor.java
+++ app/src/main/java/com/example/xavkh/browserpbe/Reproductor.java
@@ -7,10 +7,15 @@ import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import io.vov.vitamio.provider.MediaStore;
 
-public class Player extends AppCompatActivity {
+public class Reproductor extends AppCompatActivity {
 
+    private URL url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +23,18 @@ public class Player extends AppCompatActivity {
         //path will be the url of the video (can be .m3u8 or .ts)
         String path = this.getIntent().getStringExtra("url");
         //Vitameo shit here:
+        try {
+            this.url = new URL(path);
+            InputStream input = this.url.openStream();
+            URLConnection cnnct = this.url.openConnection();
+            while(input.read() != -1) {
+
+            }
+
+        } catch (Exception ex ){}
         final VideoView videoView = (VideoView) findViewById(R.id.videoView);
         videoView.setVideoURI(Uri.parse(path));
-        videoView.setMediaController(new MediaController(Player.this));
+        videoView.setMediaController(new MediaController(Reproductor.this));
         videoView.requestFocus();
         videoView.postInvalidateDelayed(100);
         new Thread(new Runnable() {
@@ -29,5 +43,7 @@ public class Player extends AppCompatActivity {
                 videoView.start();
             }
         }).start();
+
     }
+
 }
